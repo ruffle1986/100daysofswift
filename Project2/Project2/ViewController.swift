f4e6421f2fcc79c7c6df39ecd2d1cc843585e513
ruffle1986 <ftamas.mail@gmail.com>
2019-03-13 20:19:59 +0100

    project2: add game logic

--- Project2/Project2/ViewController.swift
+++ Project2/Project2/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
   
   var countries = [String]()
   var score = 0
+  var correctAnswer = 0
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -23,7 +24,7 @@ class ViewController: UIViewController {
     countries += [
       "estonia", "france", "germany", "ireland",
       "italy", "monaco", "nigeria",
-      "polad", "russia", "spain", "uk", "us"
+      "poland", "russia", "spain", "uk", "us"
     ]
     
     button1.layer.borderWidth = 1
@@ -38,11 +39,41 @@ class ViewController: UIViewController {
   }
   
   func askQuestion() {
+    countries.shuffle()
+    correctAnswer = Int.random(in: 0...2)
+    title = countries[correctAnswer].uppercased()
+    
     button1.setImage(UIImage(named: countries[0]), for: .normal)
     button2.setImage(UIImage(named: countries[1]), for: .normal)
     button3.setImage(UIImage(named: countries[2]), for: .normal)
   }
 
-
+  @IBAction func buttonTapped(_ sender: UIButton) {
+    var title:String
+    
+    if sender.tag == correctAnswer {
+      title = "Correct"
+      score += 1
+    } else {
+      title = "Wrong"
+      score -= 1
+    }
+    
+    let ac = UIAlertController(
+      title: title,
+      message: "Your score is \(score)",
+      preferredStyle: .alert
+    )
+    
+    ac.addAction(UIAlertAction(
+      title: "Continue",
+      style: .default,
+      handler: { action in
+        self.askQuestion()
+      }
+    ))
+    
+    present(ac, animated: true)
+  }
 }
 
